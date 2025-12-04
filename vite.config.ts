ee78e0126f0dbddcdeaf75cf0ae8882c77669627
mmmchu <mchu1703@gmail.com>
2025-12-04 18:08:04 +0800

    fix: update script source path in index.html and enhance vite.config.ts structure

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+
+import { defineConfig } from "vite";
+import path from "path";
+import react from "@vitejs/plugin-react";
+
 
 export default defineConfig({
-  base: '/cat-swipe/', 
+  base: '/cat-swipe/',
+  server: {
+    host: "::",
+    port: 8080,
+  },
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
+});
